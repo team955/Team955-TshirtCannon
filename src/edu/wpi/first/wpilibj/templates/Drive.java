e519bde10bde6f4097679af51183a48bcdd5314f
Ryan Bailey <ryan.lokii@gmail.com>
2012-08-12 13:31:14 -0700

    Fixed what I screwed up in last commit

---
+++ src/edu/wpi/first/wpilibj/templates/Drive.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+
+/**
+ *
+ * @author ryan
+ */
+public class Drive {
+   
+    
+    
+}
---
+++ src/edu/wpi/first/wpilibj/templates/rampingJaguar.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.*;
+/**
+ *
+ * @author ryan
+ */
+public class rampingJaguar extends Jaguar {
+    
+        public rampingJaguar (int slot, int channel) {
+            super(slot, channel);
+ 
+        }
+        
+        //public double ramp (double set){
+           // if ();
+            
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.n
+
+  
+}
